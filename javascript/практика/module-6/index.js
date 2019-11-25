494b9eabc81b6057f9526ecb06674289c9c653ed
maren4eg <pashasherstyuk@gmail.com>
2019-11-25 17:14:32 +0200

    add

--- javascript/практика/module-6/index.js
+++ javascript/практика/module-6/index.js
@@ -28,19 +28,48 @@
 
 
 
-class StringBuilder (string) {
-  constructor {
-    this._value = string
-  }
+// class StringBuilder (string) {
+//   constructor {
+//     this._value = string
+//   }
 
-  get value {
-    return this._value
-  }
+//   get value {
+//     return this._value
+//   }
 
-  append(str){
+//   append(str){
     
-  }
+//   }
+// }
+
+
+// <iframe width="560" height="315" src="https://www.youtube.com/embed/_O12WWLrUOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+////////////////
+
+
+
+/*
+  Напиши функцию-конструктор Account, которая добавляет будущему
+  объекту поля login, email. 
+  
+  В prototype функции-конструктора добавь метод getInfo(), 
+  который выводит в консоль значения полей login и email. 
+  
+  Обрати внимание, метод всего один, в поле prototype функции-конструктора, 
+  а использовать его смогут все экземпляры, по ссылке.
+  
+  Создать несколько экземпляров с разными значениями свойств, вывесди их в консоль.
+*/
+
+const Account = function(Login, Email){
+  this.Login = Login,
+  this.Email = Email
 }
 
+const account = new Account('Mangozedog', 'mango@dog.woof');
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/_O12WWLrUOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+console.log(Account.prototype.getInfo); // function
+account.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
